28f93c1226b591b4a6a1682becade23033158247
sultandaniels <63972349+sultandaniels@users.noreply.github.com>
2024-07-13 16:30:29 -0700

    trying to send observation_IR to wentinn

--- streamlined_mop/src/create_plots_with_zero_pred.py
+++ streamlined_mop/src/create_plots_with_zero_pred.py
@@ -306,8 +306,6 @@ def compute_OLS_ir(config, ys, sim_objs, max_ir_length, err_lss):
                             torch.save(obs_tensor, f)
                             print("\n\n\nsaved observation_IR tensor to file")
 
-                        print("\n printed obs_tensor:", obs_tensor)
-
                     ls.append(rls_wentinn(torch.Tensor(padded_ys[i + 1:i + ir_length + 1])[None]).squeeze(0, 1).detach().numpy())
                     ls_analytical.append(rls_wentinn.analytical_error(sim_obj).item())
 
