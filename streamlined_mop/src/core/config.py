9401fe201a57acb6070d30da36061f9dd256c00d
sultandaniels <63972349+sultandaniels@users.noreply.github.com>
2024-09-07 17:33:53 -0700

    changing to 192000 ckpt step

--- streamlined_mop/src/core/config.py
+++ streamlined_mop/src/core/config.py
@@ -30,7 +30,7 @@ class Config(object, metaclass=Singleton):
     num_traces = {"train": 1, "val": 2000}
 
     # Training settings
-    train_steps = 40000 #192000 #number of training steps
+    train_steps = 192000 #number of training steps
     train_int = 4000 #number of steps between logging
     batch_size = 28 #does changing this by 2x or 0.5x change the training time? (if no effect than batch size is too big)
     train_data_workers = 1 #set to 1 (check if it changes the speed of the training process)
