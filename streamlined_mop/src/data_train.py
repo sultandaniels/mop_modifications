c5aef6cfc1c452153d36b418d39e58ff9664fbef
sultandaniels <63972349+sultandaniels@users.noreply.github.com>
2024-09-11 20:44:59 -0700

    set ckpt path in data_train.py for --make_preds flag and removed extra space

--- streamlined_mop/src/data_train.py
+++ streamlined_mop/src/data_train.py
@@ -49,7 +49,7 @@ def preds_thread(make_preds, resume_train, train_conv):
     run_deg_kf_test = False #run degenerate KF test
     excess = False #run the excess plots
     shade = True
-    config.override("ckpt_path", "../outputs/GPT2/240910_234646.5b5972_single_system_single_system/checkpoints/step=192000.ckpt ")
+    config.override("ckpt_path", "../outputs/GPT2/240910_234646.5b5972_single_system_single_system/checkpoints/step=192000.ckpt")
 
     if resume_train:
         #get the parent directory of the ckpt_path
